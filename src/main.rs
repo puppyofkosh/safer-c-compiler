7e030ace2ec60e0230e17e7cc8049b7fec1d7022
Novopia <tomyao0@gmail.com>
2015-10-01 18:46:12 -0400

    Create Main Files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod scanner;
+mod parser;
+mod ast;
+
 fn main() {
-    println!("Hello, world!");
+    let tokens: Vec<&str> = scanner::get_tokens("return 1");
+    assert_eq!(tokens[0], "return");
+    assert_eq!(tokens[1], "1");
+    let res = parser::parse_return(tokens);
+    
 }
