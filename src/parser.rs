1715e6b6f0d573b2da42571cc35149c4f986f407
ian boros <puppyofkosh@gmail.com>
2015-11-04 12:49:29 -0500

    Changed parser to use two-stack algorithm for expressions

--- src/parser.rs
+++ src/parser.rs
@@ -5,19 +5,14 @@ use lexeme::Lexeme;
 use lexeme::OperatorType;
 use token_stream::TokenStream;
 
-fn parse_factor(tokens: &mut TokenStream) -> Expression {
-    match tokens.consume() {
-        Lexeme::Identifier(name) => Expression::Variable(name),
-        Lexeme::IntConstant(v) => Expression::Value(v),
-        Lexeme::LParen => {
-            let expr = parse_expr(tokens);
-            let last_token = tokens.consume();
-            assert_eq!(last_token, Lexeme::RParen);
-            expr
-        }
-        _ => panic!("Wth"),
-    }
-}
+
+static OPERATOR_ORDER: &'static [OperatorType] = &[
+    OperatorType::Plus, OperatorType::Minus,
+    OperatorType::CompareGreater, OperatorType::CompareLess,
+    OperatorType::CompareGreaterOrEqual, OperatorType::CompareLessOrEqual,
+    OperatorType::CompareNotEqual, OperatorType::CompareEqual,
+
+    OperatorType::Star, OperatorType::Divide];
 
 // FIXME: Do we really want to do this?
 fn optype_to_op(op: &OperatorType) -> BinaryOp {
@@ -36,77 +31,92 @@ fn optype_to_op(op: &OperatorType) -> BinaryOp {
     }
 }
 
-fn parse_term(tokens: &mut TokenStream) -> Expression {
-    let left = parse_factor(tokens);
-    if tokens.is_empty() {
-        return left;
-    }
-
-    // Check if the next operator is a * or /, in which case it is part of
-    // this term. If it's an addition operator, it's part of a expr, not a
-    // term
-    match tokens.peek() {
-        Lexeme::Operator(ref t) if (*t == OperatorType::Star) | (*t == OperatorType::Divide) => {
-            tokens.consume();
-            assert!(!tokens.is_empty(), "Last tok shouldn't be an operator");
-            let right = parse_factor(tokens);
-
-            let op = optype_to_op(t);
-            Expression::BinaryOp(op, Box::new(left), Box::new(right))
-        }
-        _ => left
-    }
+fn precedence_leq(a: &OperatorType, b: &OperatorType) -> bool {
+    OPERATOR_ORDER.iter().position(|x| x == a) < 
+        OPERATOR_ORDER.iter().position(|x| x == b)
 }
 
-fn parse_expr(tokens: &mut TokenStream) -> Expression {
-    let left = parse_term(tokens);
-    if tokens.is_empty() {
-        return left;
-    }
+fn rpn_to_ast(rpn_tokens: &Vec<Lexeme>) -> Expression {
+    let mut stack = Vec::new();
 
-    // Check if the next token is something we're interested in (an operator)
-    // If not, we're done parsing this expression
-    let op;
-    if let Lexeme::Operator(optype) = tokens.peek() {
-        op = optype_to_op(&optype);
+    for tok in rpn_tokens {
+        match *tok {
+            Lexeme::IntConstant(ref v) => stack.push(Expression::Value(*v)),
+            Lexeme::Identifier(ref name) => stack.push(Expression::Variable(name.clone())),
+            Lexeme::Operator(ref op) => {
+                let r = stack.pop().unwrap();
+                let l = stack.pop().unwrap();
+                stack.push(Expression::BinaryOp(optype_to_op(op),
+                                                Box::new(l),
+                                                Box::new(r)));
+            }
+            _ => panic!("Invalid lexeme"),
+        }
+    }
+    
+    stack.pop().unwrap()
+}
+
+fn two_stack_algo(tokens: &mut TokenStream) -> Vec<Lexeme> {
+    let mut operator_stack = Vec::new();
+    let mut output = Vec::new();
+
+    while !tokens.is_empty() {
+        let tok = tokens.peek();
+        
+        match tok {
+            Lexeme::Identifier(_) => output.push(tok),
+            Lexeme::IntConstant(_v) => output.push(tok),
+            Lexeme::Operator(o1) => {
+                while let Some(Lexeme::Operator(o2)) = operator_stack.pop() {
+                    if precedence_leq(&o1, &o2) {
+                        output.push(Lexeme::Operator(o2));
+                    }
+                    else {
+                        // push it back on the stack
+                        operator_stack.push(Lexeme::Operator(o2));
+                        break;
+                    }
+                }
+                operator_stack.push(Lexeme::Operator(o1));
+            }
+            Lexeme::LParen => operator_stack.push(tok),
+            Lexeme::RParen => {
+                while let Some(lex) = operator_stack.pop() {
+                    if lex == Lexeme::LParen {
+                        break;
+                    }
+                    
+                    output.push(lex);
+                }   
+            }
+            _ => break,
+        }
+
         tokens.consume();
-    } else {
-        return left;
     }
 
-    if tokens.is_empty() {
-        panic!("Op should not be the end of the statement");
+    while let Some(op) = operator_stack.pop() {
+        if op == Lexeme::LParen || op == Lexeme::RParen {
+            panic!("Mismatched parens");
+        }
+        
+        output.push(op);
     }
-
-    let right = parse_term(tokens);
-    Expression::BinaryOp(op, Box::new(left), Box::new(right))
+    
+    output
 }
 
-fn parse_comparison(tokens: &mut TokenStream) -> Expression {
-    let left = parse_expr(tokens);
-
-    if tokens.is_empty() {
-        return left;
-    }
-
-    if let Lexeme::Operator(optype) = tokens.peek() {
-
-        if optype != OperatorType::CompareEqual || optype != OperatorType::CompareGreater || optype != OperatorType::CompareLess || optype != OperatorType::CompareNotEqual || optype != OperatorType::CompareGreaterOrEqual || optype != OperatorType::CompareLessOrEqual {
-            return left;
-        }
-        let op = optype_to_op(&optype);
-        let right = parse_expr(tokens);
-        return Expression::BinaryOp(op, Box::new(left), Box::new(right));
-    }
-
-    left
+fn parse_expression(tokens: &mut TokenStream) -> Expression {
+    let rpn = two_stack_algo(tokens);
+    return rpn_to_ast(&rpn);
 }
 
 fn parse_return(tokens: &mut TokenStream) -> Statement {
     assert_eq!(tokens.consume(), Lexeme::Return);
     assert!(!tokens.is_empty());
 
-    let expr = parse_comparison(tokens);
+    let expr = parse_expression(tokens);
     assert_eq!(tokens.consume(), Lexeme::EndOfStatement);
     return Statement::Return(Box::new(expr));
 }
@@ -115,7 +125,7 @@ fn parse_return(tokens: &mut TokenStream) -> Statement {
 fn parse_print(tokens: &mut TokenStream) -> Statement {
     assert_eq!(tokens.consume(), Lexeme::Print);
     assert!(!tokens.is_empty());
-    let out = Statement::Print(Box::new(parse_comparison(tokens)));
+    let out = Statement::Print(Box::new(parse_expression(tokens)));
 
     assert_eq!(tokens.consume(), Lexeme::EndOfStatement);
     out
@@ -126,7 +136,7 @@ fn parse_if(tokens: &mut TokenStream) -> Statement {
     assert_eq!(tokens.consume(), Lexeme::If);
     assert!(!tokens.is_empty());
 
-    let condition = parse_comparison(tokens);
+    let condition = parse_expression(tokens);
     let block = parse_block(tokens);
 
     Statement::If(Box::new(condition), block)
@@ -136,7 +146,7 @@ fn parse_while(tokens: &mut TokenStream) -> Statement {
     assert_eq!(tokens.consume(), Lexeme::While);
     assert!(!tokens.is_empty());
 
-    let condition = parse_comparison(tokens);
+    let condition = parse_expression(tokens);
     let block = parse_block(tokens);
 
     Statement::While(Box::new(condition), block)
@@ -149,7 +159,7 @@ fn parse_declaration(tokens: &mut TokenStream) -> Statement {
     if let Lexeme::Identifier(name) = tokens.consume() {
         assert_eq!(tokens.consume(), Lexeme::Operator(OperatorType::Assign));
 
-        let expr = parse_comparison(tokens);
+        let expr = parse_expression(tokens);
         assert_eq!(tokens.consume(), Lexeme::EndOfStatement);
 
         Statement::Let(name, Box::new(expr))
@@ -164,7 +174,7 @@ fn parse_assignment(tokens: &mut TokenStream) -> Statement{
     if let Lexeme::Identifier(name) = tok {
         assert_eq!(tokens.consume(), Lexeme::Operator(OperatorType::Assign));
 
-        let expr = parse_comparison(tokens);
+        let expr = parse_expression(tokens);
         assert_eq!(tokens.consume(), Lexeme::EndOfStatement);
         
         Statement::Assign(name, Box::new(expr))
